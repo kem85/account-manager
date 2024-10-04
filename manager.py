5f174fc35b85e086cfe07385405cf442455def6a
kem <95261830+kem85@users.noreply.github.com>
2024-10-04 12:30:33 +0300

    added back feature and fixed bugs

--- manager.py
+++ manager.py
@@ -75,102 +75,113 @@ def update_scrollregion(count = 0): #update 4 scrolling
     my_canvas.configure(scrollregion=(bbox[0], bbox[1], bbox[2], max(bbox[3], min_height)))
     second_frame.update_idletasks()
     second_frame.configure(width=340, height=count*35) #50*45, 45 is y for each button and 50 is number of button
-def ADD(): #this will make it to THAT window
-    def clear(cond):
-        if catagory_var.get() == 1:
-            cun.execute('''SELECT name FROM database WHERE cata == 'false' ''')
-        else:
-            cun.execute('''SELECT name FROM database WHERE cata = ? ''',(Combo_Box.get(),))
-        namer = cun.fetchall()
-        filter=False
-        for i in range(len(namer)):
-            if name_var.get() in namer[i]:
-                filter = True
-        if cond == 0:
-            if not filter and ((len(color_var.get()) == 6) or color_var.get() == "") and name_var.get() != "" and ((email_var.get() != "" and password_var.get() !="" and Combo_Box.get() != "") or catagory_var.get()):
-                Submit.configure(state = 'disabled')
-                messagebox.showinfo(title="Successful", message="Added account successfully")
-                Submit.configure(state = 'enabled')
-                if readbase("countc") != 0 and catagory_var.get():
-                    id_var.set(f"{readbase("countc")+1}")
-                elif catagory_var.get():
-                    id_var.set("1")
-                else:
-                    count = 1
-                    cun.execute("SELECT ID FROM database WHERE name = ?", (Combo_Box.get(),))
-                    ID = cun.fetchone()[0]
-                    cun.execute("SELECT ID FROM database WHERE LENGTH(ID) >= 2")
-                    for i in cun:
-                        if i[0][0] == ID:
-                            count += 1
-                    id_var.set(f"{ID}/{count}")
-                database(name_var.get(),email_var.get(),password_var.get(),color_var.get() or "6e40c0",Combo_Box.get() or "false",id_var.get())
-                update_combobox()
-                Combo_Box.configure(values=options)
-                windowcreate('catagory',True)
+def ADD(name=""): #this will make it to THAT window
+    global Combo_Box
+    if name != "Add":
+        global state_var
+        state_var.set("LLm")
+        for widget in root.winfo_children():
+            widget.destroy()
+        default_page()
+    else:
+        def clear(cond):
+            if catagory_var.get() == 1:
+                cun.execute('''SELECT name FROM database WHERE cata == 'false' ''')
             else:
-                Submit.configure(state = 'disabled')
-                messagebox.showerror("Unsuccessful", "Invaild Format") 
-                Submit.configure(state = 'enabled')
-            name_var.set("")
-            color_var.set("")
-            email_var.set("")
-            password_var.set("")
-        elif cond==1 and catagory_var.get() == 1:
-            Combo_Box.set("")
-            Email_Label.grid_remove()
-            Email_Entry.grid_remove()
-            Password_Label.grid_remove()
-            Password_Entry.grid_remove()
-            Combo_Box.place_forget()
-        elif cond == 1 and catagory_var.get() == 0:
-            Email_Label.grid()
-            Email_Entry.grid()
-            Password_Label.grid()
-            Password_Entry.grid()
+                cun.execute('''SELECT name FROM database WHERE cata = ? ''',(Combo_Box.get(),))
+            namer = cun.fetchall()
+            filter=False
+            for i in range(len(namer)):
+                if name_var.get() in namer[i]:
+                    filter = True
+            if cond == 0:
+                if not filter and ((len(color_var.get()) == 6) or color_var.get() == "") and name_var.get() != "" and ((email_var.get() != "" and password_var.get() !="" and Combo_Box.get() != "") or catagory_var.get()):
+                    Submit.configure(state = 'disabled')
+                    messagebox.showinfo(title="Successful", message="Added account successfully")
+                    Submit.configure(state = 'enabled')
+                    if readbase("countc") != 0 and catagory_var.get():
+                        id_var.set(f"{readbase("countc")+1}")
+                    elif catagory_var.get():
+                        id_var.set("1")
+                    else:
+                        count = 1
+                        cun.execute("SELECT ID FROM database WHERE name = ?", (Combo_Box.get(),))
+                        ID = cun.fetchone()[0]
+                        cun.execute("SELECT ID FROM database WHERE LENGTH(ID) >= 2")
+                        for i in cun:
+                            if i[0][0] == ID:
+                                count += 1
+                        id_var.set(f"{ID}/{count}")
+                    database(name_var.get(),email_var.get(),password_var.get(),color_var.get() or "6e40c0",Combo_Box.get() or "false",id_var.get())
+                    update_combobox()
+                    Combo_Box.configure(values=options)
+                    if state_var.get() == "LLm":
+                        windowcreate('catagory',True)
+                    else:
+                        windowcreate(state_var.get(),True)
+                else:
+                    Submit.configure(state = 'disabled')
+                    messagebox.showerror("Unsuccessful", "Invaild Format") 
+                    Submit.configure(state = 'enabled')
+                name_var.set("")
+                color_var.set("")
+                email_var.set("")
+                password_var.set("")
+            elif cond==1 and catagory_var.get() == 1:
+                Combo_Box.set("")
+                Email_Label.grid_remove()
+                Email_Entry.grid_remove()
+                Password_Label.grid_remove()
+                Password_Entry.grid_remove()
+                Combo_Box.place_forget()
+            elif cond == 1 and catagory_var.get() == 0:
+                Email_Label.grid()
+                Email_Entry.grid()
+                Password_Label.grid()
+                Password_Entry.grid()
+                Combo_Box.place(x=269,y=260)
+        global addwindow,editwindow,buttonwindow
+        if not addwindow.get() and not editwindow.get():
+            addwindow.set(True)
+            canscroll_var.set(False)
+            addacc = Toplevel(root)
+            addacc.title("Add Account")
+            deffont = ('Helvetica', 18)
+            windowWidth = 425
+            windowHeight = 300
+            screenWidth = root.winfo_screenwidth()
+            screenHeight = root.winfo_screenheight()
+            centerX = int(screenWidth/2 - windowWidth / 2)
+            centerY = int(screenHeight/2 - windowHeight / 2)
+            addacc.geometry(f'{windowWidth}x{windowHeight}+{centerX}+{centerY}')
+            addacc.resizable(False, False)
+            addacc.protocol("WM_DELETE_WINDOW", lambda: (addwindow.set(False) , addacc.destroy(),canscroll_var.set(True)))
+            Name_Label = tb.Label(addacc, text='Name:',style=PRIMARY,font=deffont,foreground="#C0C0C0")
+            Name_Label.grid(column=0,row=0)
+            Email_Label = tb.Label(addacc, text='Email:',style=PRIMARY,font=deffont,foreground="#C0C0C0")
+            Email_Label.grid(column=0,row=1)
+            Password_Label = tb.Label(addacc, text='Pass:',style=PRIMARY,font=deffont,foreground="#C0C0C0")
+            Password_Label.grid(column=0,row=2)
+            Color_Label = tb.Label(addacc, text='Color:',style=PRIMARY,font=deffont,foreground="#C0C0C0")
+            Color_Label.grid(column=0,row=3)
+            Name_Entry = tb.Entry(addacc,width=15,textvariable=name_var,font=deffont)
+            Email_Entry = tb.Entry(addacc,width=15,font=deffont,textvariable=email_var)
+            Password_Entry = tb.Entry(addacc,width=15,font=deffont,textvariable=password_var)
+            Color_Entry = tb.Entry(addacc,width=15,font=deffont,textvariable=color_var)
+            Name_Entry.grid(column=1,row=0,pady=10,padx=5)
+            Email_Entry.grid(column=1,row=1,pady=10,padx=5)
+            Password_Entry.grid(column=1,row=2,pady=10,padx=5)
+            Color_Entry.grid(column=1,row=3,pady=10,padx=5)
+            addacc.option_add('*TCombobox*Listbox.font', ('Helvetica', 14))
+            Combo_Box = tb.Combobox(addacc,values=options,font=('Helvetica', 14),width=10)
+            style = tb.Style()
+            style.configure('Custom.TCheckbutton', font=('Helvetica', 18),foreground='#C0C0C0')
+            Catagory = tb.Checkbutton(addacc,width=10,text="Catagory",onvalue=1,offvalue=0,style='Custom.TCheckbutton',variable=catagory_var,command=lambda:clear(1))
+            Submit = tb.Button(addacc,takefocus=False,width=10,style='Custom.TButton',text="Submit",command=lambda:(clear(0)))
+            Catagory.place(x=10,y=260)
             Combo_Box.place(x=269,y=260)
-    global addwindow,editwindow,buttonwindow
-    if not addwindow.get() and not editwindow.get():
-        addwindow.set(True)
-        canscroll_var.set(False)
-        addacc = Toplevel(root)
-        addacc.title("Add Account")
-        deffont = ('Helvetica', 18)
-        windowWidth = 425
-        windowHeight = 300
-        screenWidth = root.winfo_screenwidth()
-        screenHeight = root.winfo_screenheight()
-        centerX = int(screenWidth/2 - windowWidth / 2)
-        centerY = int(screenHeight/2 - windowHeight / 2)
-        addacc.geometry(f'{windowWidth}x{windowHeight}+{centerX}+{centerY}')
-        addacc.resizable(False, False)
-        addacc.protocol("WM_DELETE_WINDOW", lambda: (addwindow.set(False) , addacc.destroy(),canscroll_var.set(True)))
-        Name_Label = tb.Label(addacc, text='Name:',style=PRIMARY,font=deffont,foreground="#C0C0C0")
-        Name_Label.grid(column=0,row=0)
-        Email_Label = tb.Label(addacc, text='Email:',style=PRIMARY,font=deffont,foreground="#C0C0C0")
-        Email_Label.grid(column=0,row=1)
-        Password_Label = tb.Label(addacc, text='Pass:',style=PRIMARY,font=deffont,foreground="#C0C0C0")
-        Password_Label.grid(column=0,row=2)
-        Color_Label = tb.Label(addacc, text='Color:',style=PRIMARY,font=deffont,foreground="#C0C0C0")
-        Color_Label.grid(column=0,row=3)
-        Name_Entry = tb.Entry(addacc,width=15,textvariable=name_var,font=deffont)
-        Email_Entry = tb.Entry(addacc,width=15,font=deffont,textvariable=email_var)
-        Password_Entry = tb.Entry(addacc,width=15,font=deffont,textvariable=password_var)
-        Color_Entry = tb.Entry(addacc,width=15,font=deffont,textvariable=color_var)
-        Name_Entry.grid(column=1,row=0,pady=10,padx=5)
-        Email_Entry.grid(column=1,row=1,pady=10,padx=5)
-        Password_Entry.grid(column=1,row=2,pady=10,padx=5)
-        Color_Entry.grid(column=1,row=3,pady=10,padx=5)
-        addacc.option_add('*TCombobox*Listbox.font', ('Helvetica', 14))
-        Combo_Box = tb.Combobox(addacc,values=options,font=('Helvetica', 14),width=10)
-        style = tb.Style()
-        style.configure('Custom.TCheckbutton', font=('Helvetica', 18),foreground='#C0C0C0')
-        Catagory = tb.Checkbutton(addacc,width=10,text="Catagory",onvalue=1,offvalue=0,style='Custom.TCheckbutton',variable=catagory_var,command=lambda:clear(1))
-        Submit = tb.Button(addacc,takefocus=False,width=10,style='Custom.TButton',text="Submit",command=lambda:(clear(0)))
-        Catagory.place(x=10,y=260)
-        Combo_Box.place(x=269,y=260)
-        Submit.place(x=135,y=260)
-def EDIT(): #same thing
+            Submit.place(x=135,y=260)
+def EDIT(name = ""): #same thing
     global editwindow,addwindow,buttonwindow
     if not editwindow.get() and not addwindow.get():
         editwindow.set(True)
@@ -206,14 +217,14 @@ def readbase(indic,id = 0):
         return count
 def windowcreate(indic,update = False): #this will make it THAT window
     global poscat_var,posbut_var,buttonss,state_var
-    if indic == 'catagory':
+    if indic == 'catagory': # creates the catagory
             if update:
                 for button in buttonss:
                     button.place_forget()
             buttonss.clear()
             cun.execute("SELECT name FROM database WHERE cata = 'false'")
             names = cun.fetchall()
-            for i in range(readbase('countc')): #45 buttons
+            for i in range(readbase('countc')):
                 button = tb.Button(second_frame, text=f'{names[i][0]}',takefocus=False,width=13,style='Custom.TButton')
                 button.configure(command=lambda b = names[i][0]: windowcreate(b))
                 if i % 2 == 0:
@@ -222,7 +233,7 @@ def windowcreate(indic,update = False): #this will make it THAT window
                     button.place(x=179, y=(i-1)*35)
                 buttonss.append(button)
             update_scrollregion()
-    elif indic == 'searchc':
+    elif indic == 'searchc': 
         global search_name
         for button in buttonss:
                  button.place_forget()
@@ -253,7 +264,10 @@ def windowcreate(indic,update = False): #this will make it THAT window
             if state_var.get() == 'LLm':
                 for widget in second_frame.winfo_children():
                     widget.place_forget()
-                windowWidth = 325
+            if update:
+                for widget in second_frame.winfo_children():
+                    widget.place_forget()
+                windowWidth = 330
                 windowHeight = 450
                 screenWidth = root.winfo_screenwidth()
                 screenHeight = root.winfo_screenheight()
@@ -261,6 +275,8 @@ def windowcreate(indic,update = False): #this will make it THAT window
                 centerY = int(screenHeight/2 - windowHeight / 2)
                 root.geometry(f'{windowWidth}x{windowHeight}+{centerX}+{centerY}')
             state_var.set(indic)
+            edit.config(text="Add",command=lambda:(ADD(name="Add"),Combo_Box.set(state_var.get())))
+            add.config(text="Back",command=lambda:ADD(name="greger"))
             cun.execute("SELECT ID FROM database WHERE name = ?", (indic,))
             id = cun.fetchone()[0]
             # subcata.resizable(False, False)
@@ -274,19 +290,17 @@ def windowcreate(indic,update = False): #this will make it THAT window
             for i in range(readbase('countb',id)):
                 button = tb.Button(second_frame, text=f'{subnames[i]}',takefocus=False,width=10,style='Custom.TButton')
                 if i % 2 == 0:
-                    button.place(x=8, y = i*35)
+                    button.place(x=4, y = i*35)
                 else:
-                    button.place(x=179, y=(i-1)*35)
+                    button.place(x=200, y=(i-1)*35)
                 buttons2.append(button)
             update_scrollregion(len(tempname))
-            
-
 addwindow = BooleanVar()
 editwindow = BooleanVar()
 #buttonwindow = BooleanVar()
 #####################################################################
 def default_page():
-    global my_canvas,second_frame
+    global my_canvas,second_frame,edit,add,state_var,main_frame
     root.title("Accounts")
     windowWidth = 335
     windowHeight = 450
@@ -307,16 +321,16 @@ def default_page():
     my_canvas.configure(yscrollcommand=my_scrollbar.set)
     second_frame = Frame(my_canvas, width=340, height=readbase('countc')*35) #50*45, 45 is y for each button and 50 is number of button
     my_canvas.create_window((0, 0), window=second_frame, anchor="nw")
+    my_canvas.bind_all("<MouseWheel>", on_mousewheel)  
     search = tb.Entry(root, textvariable=text_var,width=30)
-    edit = tb.Button(root, text='Edit',takefocus=False,width=5,style=PRIMARY,command=lambda:EDIT())
+    edit = tb.Button(root, text='Edit',takefocus=False,width=5,style=PRIMARY,command=lambda:EDIT("Edit"))
     edit.pack(side='right', anchor='e')
     search.pack(side='right', anchor='w',expand=True,padx=15,pady=5)
-    add = tb.Button(root, text='Add',takefocus=False,width=5,style=PRIMARY,command=lambda:ADD())
+    add = tb.Button(root, text='Add',takefocus=False,width=5,style=PRIMARY,command=lambda:ADD("Add"))
     add.pack(side='left', anchor='e')
     # Update scroll region to include all buttons
     windowcreate('catagory')
     update_scrollregion()
-    my_canvas.bind_all("<MouseWheel>", on_mousewheel)  
 default_page()
 # Bind mouse wheel scrollings
 text_var.trace_add("write", on_text_change)
